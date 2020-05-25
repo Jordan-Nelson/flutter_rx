1bec95fd100c01b2628f7cbd41f2d6ee1a8fe85e
Jordan Nelson <jordannelson@Jordans-MacBook-Pro.local>
2020-05-25 14:33:59 -0400

    add docs to flutter_rx

--- lib/flutter_rx.dart
+++ lib/flutter_rx.dart
@@ -2,6 +2,10 @@ import 'dart:async';
 import 'package:flutter/widgets.dart';
 import 'package:rxdart/rxdart.dart';
 
+/// Provides a [Store] to all descendants of this Widget. This should
+/// generally be a root widget in your app and your app should only
+/// conatin one [Store]. Connect to the Store provided by this Widget
+/// using `StoreProvider.of<T>(context)`.
 class StoreProvider<T> extends InheritedWidget {
   const StoreProvider({
     Key key,
@@ -24,25 +28,40 @@ class StoreProvider<T> extends InheritedWidget {
   bool updateShouldNotify(_) => false;
 }
 
+/// A base class for all Actions to extend.
+/// This allows for type safety when dispatching actions
+/// or handling actions in reducers or effects.
 class StoreAction {}
 
-/// /// A [Reducer] function that only accepts a [StoreAction] of a specific type
-typedef ActionReducer<State, Action extends StoreAction> = State Function(
-  State state,
-  Action action,
-);
-
-/// A function that takes an [StoreAction] and a [State], and returns a [State].
+/// A function that takes a [StoreAction] and a [State], and returns a new [State].
 typedef Reducer<State> = State Function(
   State state,
   StoreAction action,
 );
 
+/// A [Reducer] function that accepts a [StoreAction] of a specific type.
+typedef ActionReducer<State, Action extends StoreAction> = State Function(
+  State state,
+  Action action,
+);
+
+/// A function that handles an incoming stream of actions, optionally
+/// performs side effects upon receiving an action, and optionally returns
+/// a new `StoreAction` which will be dispatched by the store.
 typedef Effect<State> = Stream<dynamic> Function(
   BehaviorSubject<StoreAction>,
   Store<State> store,
 );
 
+/// Creates a store that holds the app state.
+///
+/// The only way to change the state in the store is to [dispatch] an
+/// action. The action will be sent to the provided [Reducer] to update the state.
+///
+/// The store will provide actions to each [Effect] after the reducer has
+/// been run and a new state has been returned.
+///
+/// The store can be provided to a widget tree using [StoreProvider].
 class Store<State> {
   Store({
     @required State initialState,
@@ -87,6 +106,18 @@ class Store<State> {
   }
 }
 
+/// A utility function that combines several [On] reducer objects.
+///
+/// ### Example
+///
+///     Reducer<int> counterReducer = createReducer([
+///       On<int, IncrementCounterAction>(
+///         (counter, action) => counter + 1,
+///       ),
+///       On<int, DecrementCounterAction>(
+///         (counter, action) => counter - 1,
+///       ),
+///     ]);
 Reducer<State> createReducer<State, Action extends StoreAction>(
   Iterable<On<State, Action>> ons,
 ) {
@@ -98,6 +129,19 @@ Reducer<State> createReducer<State, Action extends StoreAction>(
   };
 }
 
+/// A special type of reducer object that will only run for a
+/// specific [StoreAction].
+///
+/// ### Example
+///
+///     Reducer<int> counterReducer = createReducer([
+///       On<int, IncrementCounterAction>(
+///         (counter, action) => counter + 1,
+///       ),
+///       On<int, DecrementCounterAction>(
+///         (counter, action) => counter - 1,
+///       ),
+///     ]);
 class On<State, Action extends StoreAction> {
   final ActionReducer<State, Action> reducer;
 
@@ -111,6 +155,13 @@ class On<State, Action extends StoreAction> {
   }
 }
 
+/// A utility for mapping an incoming stream to a new stream. The new
+/// stream will only emit distinct values.
+///
+/// ### Example
+///     Stream<int> selectCounter(Stream<AppState> state) {
+///       return createSelector(state, (AppState state) => state.counter);
+///     }
 Stream<R> createSelector<S, R>(
   Stream<S> state,
   R Function(S) fn,
@@ -118,6 +169,19 @@ Stream<R> createSelector<S, R>(
   return state.map(fn).distinct();
 }
 
+/// A utility for mapping two incoming streams to a new stream. The new
+/// stream will only emit distinct values.
+///
+/// ### Example
+///     Stream<int> mySelector(Stream<AppState> state) {
+///       return createSelector2(
+///         mySelector1(state),
+///         mySelector2(state),
+///         (int a, int b) {
+///           return a + b;
+///         },
+///       );
+///     }
 Stream<R> createSelector2<A, B, R>(
   Stream<A> state1,
   Stream<B> state2,
@@ -126,6 +190,20 @@ Stream<R> createSelector2<A, B, R>(
   return state1.withLatestFrom(state2, fn).distinct();
 }
 
+/// A utility for mapping three incoming streams to a new stream. The new
+/// stream will only emit distinct values.
+///
+/// ### Example
+///     Stream<int> mySelector(Stream<AppState> state) {
+///       return createSelector3(
+///         mySelector1(state),
+///         mySelector2(state),
+///         mySelector3(state),
+///         (int a, int b, int c) {
+///           return a + b + c;
+///         },
+///       );
+///     }
 Stream<R> createSelector3<A, B, C, R>(
   Stream<A> state1,
   Stream<B> state2,
@@ -135,6 +213,21 @@ Stream<R> createSelector3<A, B, C, R>(
   return state1.withLatestFrom2(state2, state3, fn).distinct();
 }
 
+/// A utility for mapping four incoming streams to a new stream. The new
+/// stream will only emit distinct values.
+///
+/// ### Example
+///     Stream<int> mySelector(Stream<AppState> state) {
+///       return createSelector3(
+///         mySelector1(state),
+///         mySelector2(state),
+///         mySelector3(state),
+///         mySelector4(state),
+///         (int a, int b, int c, int d) {
+///           return a + b + c + d;
+///         },
+///       );
+///     }
 Stream<R> createSelector4<A, B, C, D, R>(
   Stream<A> state1,
   Stream<B> state2,
@@ -145,6 +238,8 @@ Stream<R> createSelector4<A, B, C, D, R>(
   return state1.withLatestFrom3(state2, state3, state4, fn).distinct();
 }
 
+/// A utility for mapping five incoming streams to a new stream. The new
+/// stream will only emit distinct values.
 Stream<R> createSelector5<A, B, C, D, E, R>(
   Stream<A> state1,
   Stream<B> state2,
@@ -156,6 +251,8 @@ Stream<R> createSelector5<A, B, C, D, E, R>(
   return state1.withLatestFrom4(state2, state3, state4, state5, fn).distinct();
 }
 
+/// A utility for mapping six incoming streams to a new stream. The new
+/// stream will only emit distinct values.
 Stream<R> createSelector6<A, B, C, D, E, F, R>(
   Stream<A> state1,
   Stream<B> state2,
@@ -170,6 +267,8 @@ Stream<R> createSelector6<A, B, C, D, E, F, R>(
       .distinct();
 }
 
+/// A utility for mapping seven incoming streams to a new stream. The new
+/// stream will only emit distinct values.
 Stream<R> createSelector7<A, B, C, D, E, F, G, R>(
   Stream<A> state1,
   Stream<B> state2,
@@ -185,6 +284,8 @@ Stream<R> createSelector7<A, B, C, D, E, F, G, R>(
       .distinct();
 }
 
+/// A utility for mapping eight incoming streams to a new stream. The new
+/// stream will only emit distinct values.
 Stream<R> createSelector8<A, B, C, D, E, F, G, H, R>(
   Stream<A> state1,
   Stream<B> state2,
@@ -210,6 +311,8 @@ Stream<R> createSelector8<A, B, C, D, E, F, G, H, R>(
       .distinct();
 }
 
+/// A utility for mapping nine incoming streams to a new stream. The new
+/// stream will only emit distinct values.
 Stream<R> createSelector9<A, B, C, D, E, F, G, H, I, R>(
   Stream<A> state1,
   Stream<B> state2,
@@ -237,6 +340,8 @@ Stream<R> createSelector9<A, B, C, D, E, F, G, H, I, R>(
       .distinct();
 }
 
+/// A utility for mapping ten incoming streams to a new stream. The new
+/// stream will only emit distinct values.
 Stream<R> createSelector10<A, B, C, D, E, F, G, H, I, J, R>(
   Stream<A> state1,
   Stream<B> state2,
