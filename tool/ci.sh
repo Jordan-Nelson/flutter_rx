ae7b6f6e0673805334a51dc0ee12c02e09e75b2a
Jordan Nelson <Jordanryannelson@gmail.com>
2022-06-12 21:36:58 -0400

    chore: CI for flutter_rx

--- tool/ci.sh
+++ tool/ci.sh
@@ -67,7 +67,11 @@ for PKG in ${PKGS}; do
       echo
       echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
       case ${TASK} in
-      analyze)
+      analyze_0)
+        echo 'flutter analyze --fatal-infos lib test'
+        flutter analyze --fatal-infos lib test || EXIT_CODE=$?
+        ;;
+      analyze_1)
         echo 'dart analyze --fatal-infos lib test'
         dart analyze --fatal-infos lib test || EXIT_CODE=$?
         ;;
@@ -76,10 +80,14 @@ for PKG in ${PKGS}; do
         dart format --output=none --set-exit-if-changed . || EXIT_CODE=$?
         ;;
       test_0)
+        echo 'flutter test'
+        flutter test || EXIT_CODE=$?
+        ;;
+      test_1)
         echo 'dart test -p chrome'
         dart test -p chrome || EXIT_CODE=$?
         ;;
-      test_1)
+      test_2)
         echo 'dart test'
         dart test || EXIT_CODE=$?
         ;;
